c3ece1505f1cbb01a62d3ef54a0f98db13ca6ef0
Wilson Bell <wilsontbell@gmail.com>
2022-08-25 09:46:43 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ch18PlanYourHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
